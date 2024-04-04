7c4746777cb385f244a7171b1c7a4e95241b4ffe
Azrael Rex Anacleto <anacletoazraelrex@gmail.com>
2024-04-05 00:54:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Shop-Galore-Ecommerce-Site
+ This is my ever serious web app. I used HTML, CSS, Tailwind and Vanilla Javascript. In this project I aimed to establish a unique Ecommerce Store that will be able to shift products worldwide in a very limited amount of time. This is good for busy customers. I will continue to improve this and then deploy it to netlify soon.
---
+++ shopgalore.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Functionality for "Buy Now" buttons
+    const buyButtons = document.querySelectorAll('.gradient');
+
+    buyButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            const product = this.parentElement.parentElement.querySelector('p.bg-pink-500').textContent;
+            alert(`You have clicked Buy Now for ${product}`);
+        });
+    });
+
+    // Functionality for navigation buttons
+    const navButtons = document.querySelectorAll('nav a');
+
+    navButtons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+            const sectionId = this.getAttribute('href').substring(1);
+            const section = document.getElementById(sectionId);
+            if (section) {
+                section.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
